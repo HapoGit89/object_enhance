8d89475857631aedd57e53c107f3d9d3a59fa872
Hannes <hannes.porombka@gmail.com>
2023-01-23 12:30:19 +0100

    finished exercise

--- object_enhancements.js
+++ object_enhancements.js
@@ -1,4 +1,4 @@
-ame keys and values
+// Same keys and values
 
 function createInstructor(firstName, lastName){
   return {
@@ -11,38 +11,60 @@ function createInstructor(firstName, lastName){
 
 /* Write an ES2015 Version */
 
-//Computed Property Names
-
-var favoriteNumber = 42;
-
-var instructor = {
-  firstName: "Colt"
+const createInstructor2 = (firstName, lastName) => {
+    return {firstName, lastName}
 }
 
-instructor[favoriteNumber] = "That is my favorite!"
+
+
+
+
+
+//Computed Property Names
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
 
 //Computed Property Names ES2015
 
 /* Write an ES2015 Version */
+let favoriteNumber = 42
 
-//Object Methods
-
-var instructor = {
-  firstName: "Colt",
-  sayHi: function(){
-    return "Hi!";
-  },
-  sayBye: function(){
-    return this.firstName + " says bye!";
-  }
+const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite"
 }
+
+// //Object Methods
+
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
 
 //Object Methods ES2015
 
 /* Write an ES2015 Version */
 
-
-
+var instructor2 = {
+  firstName: "Colt",
+  sayHi(){
+    return "Hi!";
+  },
+  sayBye(){
+    return this.firstName + " says bye!";
+  }
+}
 
 // createAnimal function
 
@@ -62,4 +84,8 @@ var instructor = {
 // // {species: "sheep", bleet: ƒ}
 // s.bleet() //"BAAAAaaaa"
 
-
+const createAnimal = (species, verb, noise) => {
+    return {species, 
+    [verb](){console.log(`${noise}`)}
+    }
+}
